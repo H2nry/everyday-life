01b2caaab0d75ccd6156b1db5c3f6e4ed57abe68
Henry <limhj@gabia.com>
2023-02-17 11:55:13 +0900

    chore: 불필요 데이터 제거

--- 한국외대/test/resource/외대_수강과목리스트불러오기-2023.1학기.js
+++ 한국외대/test/resource/외대_수강과목리스트불러오기-2023.1학기.js
@@ -1,16 +1,10 @@
 fetch("http://vsugang4.hufs.ac.kr:8080/sugang/jsp/d_sugang/d_SugangGwamokList.jsp", {
   "headers": {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "ko,en;q=0.9,en-US;q=0.8,ko-KR;q=0.7",
     "cache-control": "no-cache",
     "content-type": "application/x-www-form-urlencoded",
-    "pragma": "no-cache",
-    "upgrade-insecure-requests": "1"
   },
-  "referrer": "http://vsugang4.hufs.ac.kr:8080/sugang/jsp/d_sugang/d_SugangTop.jsp",
-  "referrerPolicy": "strict-origin-when-cross-origin",
   "body": "lang=KOR&major=D&hakgwa=DABA1",
   "method": "POST",
-  "mode": "cors",
   "credentials": "include"
 });
